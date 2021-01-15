36879c8c747630fb8d34be471839f610bac5f9e5
john beltrez <beltrezjohn@gmail.com>
2021-01-15 12:40:31 +0000

    Automatically backed up by Learn

---
+++ new.rb
@@ -0,0 +1,21 @@
+class Song
+  attr_accessor :artist, :name, :genre
+ 
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+end
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
+kiki = Song.new("In My Feelings", "hip-hop")
+
+ 
+
+ 
+kiki.artist.name
